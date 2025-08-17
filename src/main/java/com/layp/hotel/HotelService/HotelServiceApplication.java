67dbec6fe28126934cd65886596a73efb07cd478
santosh226206 <santoshamcl1996@gmail.com>
2025-08-17 12:20:24 +0530

    Hotel Service migrating from netflix eureka to consul

--- src/main/java/com/layp/hotel/HotelService/HotelServiceApplication.java
+++ src/main/java/com/layp/hotel/HotelService/HotelServiceApplication.java
@@ -2,10 +2,10 @@ package com.layp.hotel.HotelService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class HotelServiceApplication {
 
 	public static void main(String[] args) {
